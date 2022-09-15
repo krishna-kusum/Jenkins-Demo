abc748535dd95d08c2ec3b37aa5510c07931570e
Krishna Modi <59028548+krishna-kusum@users.noreply.github.com>
2022-09-15 11:02:50 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
 class Demo{
   public static void main(String args[]){
+     System.out.println("Hello from Jenkins By Java Code.......");
     while(true){
     }
-    System.out.println("Hello from Jenkins By Java Code.......");
+   
   }
 }
